82ea4e7f02431392b5f3f3c997c9afba0a7eed8b
Luis Angel Hernandez <rlwev@hotmail.com>
2024-11-25 12:01:54 -0500

    Se agregó el google tag (index.php)

--- index.php
+++ index.php
@@ -36,6 +36,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-ND224G8');</script>
 <!-- End Google Tag Manager -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-7RQ98WZE2E"></script>
+<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-7RQ98WZE2E'); </script>
+<!-- End Google tag (gtag.js) -->
 </head>
 
 <body>
